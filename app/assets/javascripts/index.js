af7b35f4c02f5b54dd9aa0049fd746fc47562030
Andrew Lever <andylever28@gmail.com>
2018-02-06 00:20:40 -0500

    minor style changes. smooth scroll implementation?

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,12 +1,24 @@
 $( document ).ready(function() {
 	$(window).on('scroll',function(){
 		if ($(window).scrollTop() >= 100) {
-	     $('#mainNav').css("background-color", "#343a40"); 
+			$('#mainNav').css("background-color", "#343a40"); 
+			$("#bounce-arrow").hide();
 	 	} else { 
 	   	$('#mainNav').css("background-color", "transparent"); 
+	   	$("#bounce-arrow").show();
 		}
 	});
-
+	$('#bounce-arrow').css('width', '100%');
 	$("#bounce-arrow").show();
+
+	$('a[href^="#"]').on('click', function() {
+		var target = this.hash; 
+		$target = $(target);
+		$('html, body').stop().animate({
+        'scrollTop':  parseInt($target.offset().top,10)
+    }, 900, 'swing', function () {
+        window.location.hash = target;
+    });
+	})
 });
 
