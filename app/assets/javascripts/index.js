8364675bf9508b5bddd31c73df58b8b05b6a781c
Andrew Lever <andylever28@gmail.com>
2018-02-21 02:23:01 -0500

    add new links

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,19 +1,19 @@
 $( document ).ready(function() {
 
 	$(window).on('scroll',function(){
-		if ($(window).scrollTop() >= 100) {
+		if ($(window).scrollTop() >= 100 || $(".navbar-toggler").hasClass("collapsed") === false) {
 			$('#mainNav').css("background-color", "#343a40"); 
 			$("#bounce-arrow").hide();
 	 	} else { 
-	   	$('#mainNav').css("background-color", "transparent"); 
-	   	$("#bounce-arrow").show();
+			$('#mainNav').css("background-color", "transparent"); 
+			$("#bounce-arrow").show();
 		}
 	});
 
 	$('#bounce-arrow').css('width', '100%');
 	$("#bounce-arrow").show();
 
-	$('.nav-link').on('click', function(e) {
+	$('.scroll-link').on('click', function(e) {
     e.preventDefault();
     $('#navbarNav').removeClass('show');
 		$('#mainNav').css("background-color", "#343a40"); 
@@ -27,5 +27,14 @@ $( document ).ready(function() {
     });
 	})
 
+	var navFirstTimeOpened = true
+	$(".navbar-toggler").on("click", function() {
+		if ($(this).hasClass("collapsed") || navFirstTimeOpened) {
+			$('#mainNav').css("background-color", "#343a40"); 
+			navFirstTimeOpened = false;
+		} else { 
+			$('#mainNav').css("background-color", "transparent"); 
+		} 
+	})
 });
 
